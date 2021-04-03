3e28df953867b562acb1c4a73397cfe915afd57b
TheSkyShine <77767794+TheSkyShine@users.noreply.github.com>
2021-04-03 16:11:28 -0600

    Update Motor_Prototype_1.0.ino

--- Motor_Prototype_1.0.ino
+++ Motor_Prototype_1.0.ino
@@ -12,7 +12,7 @@
 Servo servo360Micro1_1;
 Servo servo360Micro2_2;
 
-VCNL4040 proximitySensor;
+VCNL4040 proximitySensor1;
 VCNL4040 proximitySensor2;
 
 boolean somethingthere = false;
@@ -24,22 +24,32 @@ void setup() {
   Serial.println("Begin the test");
   Wire.begin();
 
-  if (proximitySensor.begin() == false)
+  if (proximitySensor1.begin() == false)
   {
-    Serial.println("Not connected");
+    Serial.println("Sensor 1 not connected");
+    while (1);
+  }
+
+  if (proximitySensor2.begin() == false)
+  {
+    Serial.println("Sensor 2 not connected");
     while (1);
   }
   
-  proximitySensor.setLEDCurrent(200);
-  proximitySensor.setProxIntegrationTime(8);
+  proximitySensor1.setLEDCurrent(200);
+  proximitySensor1.setProxIntegrationTime(8);
+
+  proximitySensor2.setLEDCurrent(200);
+  proximitySensor2.setProxIntegrationTime(8);
 }
 
 void loop() {
-  unsigned int proxValue = proximitySensor.getProximity();
+  unsigned int proxValue1 = proximitySensor1.getProximity();
+  unsigned int proxValue2 = proximitySensor2.getProximity();
 
   Serial.print("I don't see anything");
 
-  if(proxValue > 1)
+  if((proxValue1 > 1) || (proxValue2 > 1) )
   {
     Serial.print("\nSomething is there");
     somethingthere = true;
@@ -65,12 +75,12 @@ void loop() {
     motor2_counter++;
   }
 
-  if(millis() == 60000) {
+  if(millis() > 1000 && millis() < 11000) {
     servo360Micro2_2.attach(SERVO360MICRO2_2_PIN_SIG);
     servo360Micro2_2.write(110);
     delay(1000);
     servo360Micro2_2.write(90);
-    delay(10);
+    delay(100);
     servo360Micro2_2.write(70);
     delay(1000);
     servo360Micro2_2.detach(); 
